2b89e25d2ab8e0e69b8130e53234450df130dc99
DEBOERAH <eniolaofuani@gmail.com>
2024-10-16 12:52:47 +0100

    javascript assignment

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function reverse(arr){
+    return arr.split("").reverse().join("");
+
+}
+reverse("my name is adaeze");
+
+
+function Count(arr){
+return arr.split("").length;
+
+
+
+}
+Count("ADAEZE")
+
+function capi(arr){
+return arr.split("").toUpperCase();
+
+}
+capi("dffjk")
